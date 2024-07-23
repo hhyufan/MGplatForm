1054551af988d6ced90260da5d0f71d0e9a80704
hhyufan <1838248655@qq.com>
2024-07-24 01:21:06 +0800

    推送 refactor: 给user_info_register表的id改为自增主键

--- src/main/resources/InitPlatform.sql
+++ src/main/resources/InitPlatform.sql
@@ -13,4 +13,8 @@ CREATE TABLE user_info_register(
     id int PRIMARY KEY ,
     username varchar(255),
     data varchar(255)
-)
+);
+# ALTER TABLE user_info_register DROP PRIMARY KEY;
+# ALTER TABLE user_info_register
+#     MODIFY COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
+# INSERT INTO global (id, section, data)  VALUES (1, 'current', 'testuser@example.com');
