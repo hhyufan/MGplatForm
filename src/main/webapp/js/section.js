74f4935d3b38ccd5198cb1fa47f4de3d1dcafd7f
hhyufan <1838248655@qq.com>
2024-07-23 18:06:42 +0800

    feat：增加侧边栏切换时的section的选择性显示

---
+++ src/main/webapp/js/section.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const homeLink = document.querySelector('.nav a[href="#"]'); // 首页链接
+  const personalCenterLink = document.getElementById('personal-center-link'); // 个人中心链接
+  const mainContentSection = document.getElementById('main-content-section'); // 首页 section
+  const personalCenterSection = document.getElementById('personal-center-section'); // 个人中心 section
+
+  // 点击首页链接时显示首页 section
+  homeLink.addEventListener('click', function(event) {
+    event.preventDefault(); // 阻止默认行为
+    mainContentSection.style.display = 'block';
+    //window.location.href = "http://localhost:8080/MGManagePlatForm_war/website.html";
+    personalCenterSection.style.display = 'none';
+  });
+
+  // 点击个人中心链接时显示个人中心 section
+  personalCenterLink.addEventListener('click', function(event) {
+    event.preventDefault(); // 阻止默认行为
+    mainContentSection.style.display = 'none';
+    personalCenterSection.style.display = 'block';
+  });
+});
