ba35b29e9bff44e81eabb934b50d4c5c5e07711f
hhyufan <1838248655@qq.com>
2024-07-23 19:23:32 +0800

    推送 feat:为所有暂未开发功能增加"功能未开发"加载动画

--- src/main/webapp/js/section.js
+++ src/main/webapp/js/section.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const personalCenterLink = document.getElementById('personal-center-link'); // 个人中心链接
   const mainContentSection = document.getElementById('main-content-section'); // 首页 section
   const personalCenterSection = document.getElementById('personal-center-section'); // 个人中心 section
-
+  const chartLine  = document.getElementById('chart-line');
+  const users = document.getElementById('users');
+  const envelope = document.getElementById('envelope');
   // 点击首页链接时显示首页 section
   homeLink.addEventListener('click', function(event) {
     event.preventDefault(); // 阻止默认行为
@@ -18,4 +20,10 @@ document.addEventListener('DOMContentLoaded', function() {
     mainContentSection.style.display = 'none';
     personalCenterSection.style.display = 'block';
   });
+  const hrefLoading = () => {
+    window.location.href = 'http://localhost:8080/MGManagePlatForm_war/loading.html';
+  }
+  chartLine.addEventListener('click', hrefLoading)
+  users.addEventListener('click', hrefLoading)
+  envelope.addEventListener('click', hrefLoading)
 });
