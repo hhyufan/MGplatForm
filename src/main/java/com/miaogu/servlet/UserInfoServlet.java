d0a7a62abf63736ce193ac2f84f7962fea541055
hhyufan <1838248655@qq.com>
2024-07-24 15:11:56 +0800

    推送 feat: 增加website前端标题动态显示，某用户为管理员的信息

--- src/main/java/com/miaogu/servlet/UserInfoServlet.java
+++ src/main/java/com/miaogu/servlet/UserInfoServlet.java
@@ -1,12 +1,24 @@
 package com.miaogu.servlet;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.miaogu.dao.GlobalDao;
+import com.miaogu.dao.GlobalDaoImpl;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
 @WebServlet("/userInfo")
 public class UserInfoServlet extends HttpServlet {
+    private final GlobalDao globalDao;
+    public UserInfoServlet() {
+        globalDao = new GlobalDaoImpl();
+    }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -14,8 +26,24 @@ public class UserInfoServlet extends HttpServlet {
         String username = (session != null) ? (String) session.getAttribute("username") : null;
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("{\"username\": \"" + (username != null ? username : "") + "\"}");
+        JsonObject jsonResponse = getJsonObject(username);
+        Gson gson = new Gson();
+        String json = gson.toJson(jsonResponse);
+        response.getWriter().write(json);
     }
+
+    private JsonObject getJsonObject(String username) {
+        JsonObject jsonResponse = new JsonObject();
+        List<String> administrators = globalDao.getAdministrator();
+        administrators.forEach(
+                administrator -> {
+                    jsonResponse.addProperty("isAdministrator", administrator.equals(username));
+                }
+        );
+        jsonResponse.addProperty("username", username);
+        return jsonResponse;
+    }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         doPost(request, response);
--- src/main/webapp/js/getName.js
+++ src/main/webapp/js/getName.js
@@ -12,10 +12,10 @@ fetch('http://localhost:8080/MGManagePlatForm_war/userInfo', {
     .then(data => {
         // 假设后端返回的数据结构包含用户名字段
         const username = data.username;
-
+        const isAdministrator = data.isAdministrator;
         // 选择页面中的h2元素，并将用户名设置为其文本内容
         const h2Title = document.querySelector('h2');
-        h2Title.textContent = `你好, ${username}! 欢迎来到喵咕平台`;
+        h2Title.textContent = `你好${isAdministrator ? "管理员": ""}, ${username}! 欢迎来到喵咕平台`;
     })
     .catch(error => {
         console.error('Error fetching user info:', error);
