0266491a6d60995da7bdd77673ca97c6ea7beccd
hhyufan <1838248655@qq.com>
2024-07-24 08:52:01 +0800

    推送 feat：增加platForm.html注释

--- src/main/webapp/js/theme.js
+++ src/main/webapp/js/theme.js
@@ -1,18 +1,19 @@
-// 要操作到的元素
 let login=document.getElementById('login');
 let register=document.getElementById('register');
 let form_box=document.getElementsByClassName('form-box')[0];
 let register_box=document.getElementsByClassName('register-box')[0];
 let login_box=document.getElementsByClassName('login-box')[0];
-// 去注册按钮点击事件
+
+// 去注册
 register.addEventListener('click', () => {
     form_box.style.transform = 'translateX(80%)';
     form_box.style.background = '#d3b7d8';
-    login_box.classList.add('hidden');
+    login_box.classList.add('hidden'); //添加隐藏类名，隐藏表单
     register_box.classList.remove('hidden');
-    document.body.style.background = 'linear-gradient(200deg,#e3c5eb,#a9c1ed)';
+    document.body.style.background = 'linear-gradient(200deg,#e3c5eb,#a9c1ed)'; //设置渐变效果
 });
 
+// 去登录
 login.addEventListener('click', () => {
     form_box.style.transform = 'translateX(0%)';
     form_box.style.background = '#ebc777';
