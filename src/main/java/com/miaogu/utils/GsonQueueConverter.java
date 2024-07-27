05518d7b86d5098da8e7e2579031bbc115bc8221
hhyufan <1838248655@qq.com>
2024-07-27 23:16:11 +0800

    feat:实现Queue接口为MyQueue，增加Queue和Gson的转换工具类

---
+++ src/main/java/com/miaogu/utils/GsonQueueConverter.java
@@ -0,0 +1,31 @@
+package com.miaogu.utils;
+
+import com.google.gson.Gson;
+import org.jetbrains.annotations.NotNull;
+
+public class GsonQueueConverter {
+
+    /**
+     * 将 Gson 字符串转换为队列
+     * @param gsonArrayJson Gson 数组的 JSON 字符串
+     * @return 转换后的队列
+     */
+    public static MyQueue<String> fromGsonArray(String gsonArrayJson) {
+        Gson gson = new Gson();
+        String[] gsonArray = gson.fromJson(gsonArrayJson, String[].class);
+
+        MyQueue<String> queue = new MyQueue<>();
+        for (String element : gsonArray) {
+            queue.offer(element);
+        }
+
+        return queue;
+    }
+
+    public static String toGsonArray(MyQueue<String> queue) {
+        Gson gson = new Gson();
+        String[] gsonArray = queue.toArray(new String[0]);
+        return gson.toJson(gsonArray);
+    }
+
+}
---
+++ src/main/java/com/miaogu/utils/MyQueue.java
@@ -0,0 +1,115 @@
+package com.miaogu.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+public class MyQueue<E> implements java.util.Queue<E> {
+
+    private ArrayDeque<E> deque;
+
+    public MyQueue() {
+        deque = new ArrayDeque<>();
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (deque.isEmpty()) {
+            deque.add(e);
+            deque.add(e); // 添加当前值两次
+            return true;
+        } else if (deque.size() == 2) {
+            deque.remove(); // 移除队首元素
+            deque.add(e); // 将当前值添加到队尾
+            return true;
+        } else {
+            return deque.add(e);
+        }
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return deque.offer(e);
+    }
+
+    @Override
+    public E remove() {
+        return deque.remove();
+    }
+
+    @Override
+    public E poll() {
+        return deque.poll();
+    }
+
+    @Override
+    public E element() {
+        return deque.element();
+    }
+
+    @Override
+    public E peek() {
+        return deque.peek();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return deque.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return deque.size();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return deque.contains(o);
+    }
+
+    @Override
+    public @NotNull Iterator<E> iterator() {
+        return deque.iterator();
+    }
+
+    @Override
+    public Object @NotNull [] toArray() {
+        return deque.toArray();
+    }
+
+    @Override
+    public <T> T @NotNull [] toArray(T[] a) {
+        return deque.toArray(a);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return deque.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(java.util.Collection<?> c) {
+        return deque.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(java.util.Collection<? extends E> c) {
+        return deque.addAll(c);
+    }
+
+    @Override
+    public boolean removeAll(java.util.Collection<?> c) {
+        return deque.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(java.util.Collection<?> c) {
+        return deque.retainAll(c);
+    }
+
+    @Override
+    public void clear() {
+        deque.clear();
+    }
+}
